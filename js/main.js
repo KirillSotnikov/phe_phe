55677951973e6daaf9b5c49feae3595d3f327a41
Programmer_kirill <programmer_kirill@g0ncharuk.local>
2019-01-08 17:24:09 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $('.header_menu').click(function(){
     $('.menu_container').toggleClass('active');
 });
+$('.memories_slider').slick({
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,  
+    // arrows: false,  
+    // asNavFor: '.big_slider .slider',
+    prevArrow: '<div class="arrow left"><img src="img/arrow.png"></div>',
+    nextArrow: '<div class="arrow right"><img src="img/arrow.png"></div>',
+
+});
+
+var windowWidth = $(document).width();
+console.log(windowWidth);
+
+if( windowWidth <= 800 ){
+    $('.header').removeClass('transparent_head');
+}
