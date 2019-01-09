76e71e9e36b8ff24e718cec16395b901cbc1267c
Programmer_kirill <programmer_kirill@g0ncharuk.local>
2019-01-09 11:44:19 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -15,18 +15,19 @@ $('.memories_slider').slick({
 var windowWidth = $(document).width();
 // console.log(windowWidth);
 
-if( windowWidth <= 800 ){
+if( windowWidth <= 850 ){
     $('.header').removeClass('transparent_head');
 }
 
-
-
-$(window).on('mousewheel', function(){
+var scroolHeader = function(){
     var scroollEventer = $('html').scrollTop();
     console.log(scroollEventer);
     if(scroollEventer >= 100){
         $('.header').removeClass('transparent_head');
-    } else if (scroollEventer >= 100 && windowWidth >= 800){
+    } else if (scroollEventer <= 100 && windowWidth >= 850){
         $('.header').addClass('transparent_head');
     }
-}); 
+}
+$(window).on('load', scroolHeader);
+$(window).scroll(scroolHeader);
+// $(window).on('mousewheel',scroolHeader); 
